f2624fcce17671ed01e10f059c5b4daa1abf5824
fgonzalezc <>
2020-09-24 00:18:02 -0500

    {/src/main/java/com/learning/testingcasemgmt2/Empleado.java}

--- src/main/java/com/learning/testingcasemgmt2/Empleado.java
+++ src/main/java/com/learning/testingcasemgmt2/Empleado.java
@@ -7,29 +7,37 @@ package com.learning.testingcasemgmt2;
 @javax.persistence.Entity
 public class Empleado implements java.io.Serializable {
 
-    static final long serialVersionUID = 1L;
+	static final long serialVersionUID = 1L;
 
-    @javax.persistence.GeneratedValue(strategy = javax.persistence.GenerationType.AUTO, generator = "EMPLEADO_ID_GENERATOR")
-    @javax.persistence.Id
-    @javax.persistence.SequenceGenerator(name = "EMPLEADO_ID_GENERATOR", sequenceName = "EMPLEADO_ID_SEQ")
-    private java.lang.Long id;
+	@javax.persistence.GeneratedValue(strategy = javax.persistence.GenerationType.AUTO, generator = "EMPLEADO_ID_GENERATOR")
+	@javax.persistence.Id
+	@javax.persistence.SequenceGenerator(name = "EMPLEADO_ID_GENERATOR", sequenceName = "EMPLEADO_ID_SEQ")
+	private java.lang.Long id;
 
-    public Empleado() {
-    }
-    
-    public Empleado(java.lang.Long id) {
-        this.id = id;
-    }
+	private java.lang.String nombre;
 
-    public java.lang.Long getId() {
-        return this.id;
-    }
-    
-    public void setId(java.lang.Long id) {
-        this.id = id;
-    }
+	public Empleado() {
+	}
 
+	public java.lang.Long getId() {
+		return this.id;
+	}
 
+	public void setId(java.lang.Long id) {
+		this.id = id;
+	}
 
+	public java.lang.String getNombre() {
+		return this.nombre;
+	}
+
+	public void setNombre(java.lang.String nombre) {
+		this.nombre = nombre;
+	}
+
+	public Empleado(java.lang.Long id, java.lang.String nombre) {
+		this.id = id;
+		this.nombre = nombre;
+	}
 
 }
